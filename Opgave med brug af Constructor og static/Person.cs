e1bc775a2feeec5e21c38440f1fd7a08a09ea9ce
Kamilek2000PL <Kamilek2000PL@DESKTOP-7OBE0OB>
2023-05-23 22:05:17 +0200

    Add project files.

---
+++ Opgave med brug af Constructor og static/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Person
+{
+    private static int instanceCount = 0;
+
+    public int ID { get; private set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string PostalCode { get; set; }
+    public string City { get; set; }
+
+    public Person()
+    {
+        instanceCount++;
+        ID = instanceCount;
+    }
+
+    public static void WritePersonInstance(Person person)
+    {
+        Console.WriteLine($"ID: {person.ID}");
+        Console.WriteLine($"Adres: {person.Address}");
+    }
+
+    public static void WriteInstance(Person person)
+    {
+        Console.WriteLine($"ID: {person.ID}");
+        Console.WriteLine($"Navn: {person.FirstName} {person.LastName}");
+        Console.WriteLine($"Samlet antal forekomster: {instanceCount}");
+    }
+}
---
+++ Opgave med brug af Constructor og static/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person1 = new Person();
+        person1.FirstName = "Jan";
+        person1.LastName = "Jensen";
+        person1.Address = "Telegrafvej 9";
+        person1.PostalCode = "0000";
+        person1.City = "Ballerup";
+
+        Person person2 = new Person();
+        person2.FirstName = "Thomas";
+        person2.LastName = "Val";
+        person2.Address = "Vangede 1";
+        person2.PostalCode = "1203";
+        person2.City = "Gentofte";
+
+        Console.WriteLine("Personoplysninger:");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("Person 1:");
+        Person.WritePersonInstance(person1);
+        Console.WriteLine();
+        Console.WriteLine("Person 2:");
+        Person.WritePersonInstance(person2);
+        Console.WriteLine();
+
+        Console.WriteLine("Forekomstoplysninger:");
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Person 1:");
+        Person.WriteInstance(person1);
+        Console.WriteLine();
+        Console.WriteLine("Person 2:");
+        Person.WriteInstance(person2);
+
+        Console.ReadLine(); // Dodana linia, aby aplikacja zatrzymała się przed zakończeniem
+    }
+}
